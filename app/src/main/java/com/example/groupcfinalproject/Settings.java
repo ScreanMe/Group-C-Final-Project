1dd63431aef2ec953c772798699144abedb2d149
Daniyal <mixer126@mail.ru>
2021-04-11 18:35:28 -0700

    Added clear all data to expenses and settings

--- app/src/main/java/com/example/groupcfinalproject/Settings.java
+++ app/src/main/java/com/example/groupcfinalproject/Settings.java
@@ -1,15 +1,20 @@
 package com.example.groupcfinalproject;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class Settings extends AppCompatActivity {
+
+    DatabaseHelper databaseHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +35,30 @@ public class Settings extends AppCompatActivity {
         public void onNothingSelected(AdapterView<?> parent) {
             // Another interface callback
         }
+
     }
-    
+
+    public void clear(View view) {
+        databaseHelper = new DatabaseHelper(Settings.this);
+        DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which){
+                    case DialogInterface.BUTTON_POSITIVE:
+                        databaseHelper.deleteAll();
+                        Toast.makeText(Settings.this, "All Data Cleared", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case DialogInterface.BUTTON_NEGATIVE:
+                        dialog.dismiss();
+                        break;
+                }
+            }
+        };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(Settings.this);
+        builder.setMessage("Are you sure?").setPositiveButton("Yes", dialogClickListener)
+                .setNegativeButton("No", dialogClickListener).show();
+    }
 
 }
